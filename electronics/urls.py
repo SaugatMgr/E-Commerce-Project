b7b429e9f179e2a165c211c319b36a87886dbb83
SaugatMgr <saugatmagar433@gmail.com>
2023-07-24 14:44:11 +0545

    url for about and contact page

--- electronics/urls.py
+++ electronics/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import(
+    HomePageView,
+    AboutUsPageView,
+    ContactUsPageView,
+)
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutUsPageView.as_view(), name="about"),
+    path("contact/", ContactUsPageView.as_view(), name="contact"),
 ]
