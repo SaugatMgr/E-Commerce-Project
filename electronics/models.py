2f10c20502ba0a6a7911b3cbc8743f67aae3986b
SaugatMgr <saugatmagar433@gmail.com>
2023-07-25 08:17:31 +0545

    str method for image model

--- electronics/models.py
+++ electronics/models.py
@@ -14,3 +14,7 @@ class Product(models.Model):
 class Image(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     images = models.ImageField(upload_to="product_images/")
+    
+    def __str__(self):
+        return self.product.name
+    
