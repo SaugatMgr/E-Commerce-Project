2d4538be115a07a81ff50d046ae1f9ffd939b40b
SaugatMgr <saugatmagar433@gmail.com>
2023-07-25 08:03:03 +0545

    Product Model

--- electronics/models.py
+++ electronics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    product_img_thumbnail = models.ImageField(upload_to="product/", blank=True)
+
+    def __str__(self):
+        return self.name
+    
