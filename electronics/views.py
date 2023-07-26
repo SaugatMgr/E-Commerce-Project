00711291481c9b5bff091c776de21ae61815fd39
SaugatMgr <saugatmagar433@gmail.com>
2023-07-26 15:17:11 +0545

    home page view slider section left side category part logic

--- electronics/views.py
+++ electronics/views.py
@@ -1,11 +1,35 @@
-from django.views.generic import ListView, TemplateView
+from django.views.generic import (
+    ListView,
+    TemplateView,
+)
+from multi_form_view import MultiModelFormView
 
-from .models import Product
+
+from .models import (
+    Product,
+    Image,
+    Category,
+    SubCategory,
+)
+from .forms import (
+    AddProductForm,
+    AddImagesForm,
+)
 
 
 class HomePageView(ListView):
     model = Product
     template_name = "main/home/home.html"
+    context_object_name = "products"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["product_category"] = Category.objects.all()[:9]
+        context["product_category_rem"] = Category.objects.all()[9:12]
+        context["product_sub_category"] = SubCategory.objects.all()
+
+        return context
 
 
 class AboutUsPageView(TemplateView):
