4bf618369db4897bf094cad949196471e79246e8
SaugatMgr <saugatmagar433@gmail.com>
2023-07-26 15:19:09 +0545

    added required models and fields

--- electronics/models.py
+++ electronics/models.py
@@ -1,11 +1,46 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=64)
+    category = models.ForeignKey(
+        Category,
+        default="",
+        on_delete=models.CASCADE
+    )
+    
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=150)
     price = models.IntegerField()
     description = models.TextField()
     product_img_thumbnail = models.ImageField(upload_to="product/", blank=True)
+    added_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_date = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    category = models.ForeignKey(
+        Category,
+        default="",
+        on_delete=models.CASCADE
+    )
+
+    tag = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
