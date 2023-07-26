1566c5d703469cb3ef870a1359695673df74ac23
SaugatMgr <saugatmagar433@gmail.com>
2023-07-26 15:18:41 +0545

    registered category, sub-category and tag model to admin site

--- electronics/admin.py
+++ electronics/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from .models import Product, Image
+from .models import (
+    Product,
+    Image,
+    Category,
+    SubCategory,
+    Tag,
+)
 
 admin.site.register(Product)
 admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Tag)
