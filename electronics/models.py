c2083557ebd4b668c7e79c1ff5a67af277a41321
SaugatMgr <saugatmagar433@gmail.com>
2023-07-26 08:48:09 +0545

    altered price field

--- electronics/models.py
+++ electronics/models.py
@@ -3,18 +3,17 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=150)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.IntegerField()
     description = models.TextField()
     product_img_thumbnail = models.ImageField(upload_to="product/", blank=True)
 
     def __str__(self):
         return self.name
-    
-    
+
+
 class Image(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     images = models.ImageField(upload_to="product_images/")
-    
+
     def __str__(self):
         return self.product.name
-    
