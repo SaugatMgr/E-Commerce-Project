ccc29c6d6ea063505630d498b447ced16d98060d
SaugatMgr <saugatmagar433@gmail.com>
2023-07-27 11:30:46 +0545

    best seller products filter

--- electronics/views.py
+++ electronics/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import (
-    ListView,
     TemplateView,
+    ListView,
+    DetailView,
 )
 from multi_form_view import MultiModelFormView
 
@@ -26,12 +27,15 @@ class HomePageView(ListView):
         context = super().get_context_data(**kwargs)
 
         all_categories = Category.objects.all()
-        
+        filter_product = Product.objects.filter(views_count__gte=0).order_by("-views_count")
+             
         context["categories"] = all_categories
         context["product_category"] = all_categories[:9]
         context["product_category_rem"] = all_categories[9:12]
         context["product_sub_category"] = SubCategory.objects.all()
 
+        context["best_sellers"] = filter_product
+        
         return context
 
 
@@ -41,3 +45,9 @@ class AboutUsPageView(TemplateView):
 
 class ContactUsPageView(TemplateView):
     template_name = "contact_us.html"
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "main/home/product/product detail/product_details.html"
+    context_object_name = "product"
+    
