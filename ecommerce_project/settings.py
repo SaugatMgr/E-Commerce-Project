8af8afb631776031c7ba755b0de0457c2ab09d7a
SaugatMgr <saugatmagar433@gmail.com>
2023-07-27 13:13:22 +0545

    humanize app added to format numbers

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     # Local Apps
     'accounts.apps.AccountsConfig',
     'electronics.apps.ElectronicsConfig',
