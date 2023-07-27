9d71d4acced64255ae2eb36b95209aedda486fd1
SaugatMgr <saugatmagar433@gmail.com>
2023-07-27 23:17:14 +0545

    Review and Contact Models

--- electronics/models.py
+++ electronics/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+from django.contrib.auth import get_user_model
+
+CustomUser = get_user_model()
+
+
+class TimeStamp(models.Model):
+    added_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_date = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class NameMsgEmail(models.Model):
+    name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=150)
+    msg = models.TextField()
+
+    class Meta:
+        abstract = True
 
 
 class Category(models.Model):
@@ -9,6 +29,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class SubCategory(models.Model):
     name = models.CharField(max_length=64)
     category = models.ForeignKey(
@@ -16,7 +37,7 @@ class SubCategory(models.Model):
         default="",
         on_delete=models.CASCADE
     )
-    
+
     def __str__(self):
         return self.name
 
@@ -28,15 +49,13 @@ class Tag(models.Model):
         return self.name
 
 
-class Product(models.Model):
+class Product(TimeStamp):
     name = models.CharField(max_length=150)
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
     price = models.IntegerField()
     description = models.TextField()
     product_img_thumbnail = models.ImageField(upload_to="product/", blank=True)
     views_count = models.PositiveBigIntegerField()
-    added_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    updated_date = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     category = models.ForeignKey(
         Category,
@@ -57,6 +76,29 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("product_detail", kwargs={"slug": self.slug})
+
+
+class Review(NameMsgEmail, TimeStamp):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="reviews"
+    )
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.user}: {self.msg[:30]}"
+
+
+class Contact(NameMsgEmail, TimeStamp):
+    subject = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.user}: {self.msg[:30]}"
+
 
 class Image(models.Model):
     product = models.ForeignKey(
