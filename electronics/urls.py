d332d9a5ecc8e1812578a888eefc55e860dc9a88
SaugatMgr <saugatmagar433@gmail.com>
2023-07-27 13:19:18 +0545

    product detail url

--- electronics/urls.py
+++ electronics/urls.py
@@ -4,10 +4,12 @@ from .views import(
     HomePageView,
     AboutUsPageView,
     ContactUsPageView,
+    ProductDetailView,
 )
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutUsPageView.as_view(), name="about"),
     path("contact/", ContactUsPageView.as_view(), name="contact"),
+    path("product-detail/<slug:slug>/", ProductDetailView.as_view(), name="product_detail"),
 ]
