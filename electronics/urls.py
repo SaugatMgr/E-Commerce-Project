3a35d1df7258aab8f40adb2fd036e9f9cbb7b0c2
SaugatMgr <saugatmagar433@gmail.com>
2023-08-08 20:42:07 +0545

    create product url

--- electronics/urls.py
+++ electronics/urls.py
@@ -8,14 +8,21 @@ from .views import(
     ReviewView,
     NewsLetterView,
     ShopItemsView,
+    AddProductView,
 )
 
 urlpatterns = [
+    # different pages url
     path("", HomePageView.as_view(), name="home"),
     path("shop/", ShopItemsView.as_view(), name="shop"),
     path("about/", AboutUsPageView.as_view(), name="about"),
     path("contact/", ContactUsPageView.as_view(), name="contact"),
+    # product detail url
     path("product-detail/<slug:slug>/", ProductDetailView.as_view(), name="product_detail"),
+    # product review url
     path("review/", ReviewView.as_view(), name="review"),
+    # newsletter url
     path("newsletter/", NewsLetterView.as_view(), name="newsletter"),
+    # url for CRUD operations on Product
+    path("create/", AddProductView.as_view(), name="add_product"),
 ]
