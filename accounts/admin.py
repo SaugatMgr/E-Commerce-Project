dde0029cf79c8ec4be6055179a23087dade525c2
SaugatMgr <saugatmagar433@gmail.com>
2023-08-10 17:41:46 +0545

    new fields first name and last name added to CustomUserAdmin + new CustomerAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,20 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .forms import (
+    CustomUserCreationForm,
+    CustomUserChangeForm,
+    CustomerForm,
+)
+from .models import CustomUser, Customer
 
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ("email", "is_staff", "is_active", )
-    list_filter = ("email", "is_staff", "is_active", )
+    list_display = ("email", "first_name", "last_name", "is_staff", "is_active", )
+    list_filter = ("email", "first_name", "last_name", "is_staff", "is_active", )
     fieldsets = (
         (None, {
             "fields": (
-                "email", "password"
+                "email", "first_name", "last_name", "password", 
             ),
         }),
         ("Permissions", {
@@ -27,7 +31,7 @@ class CustomUserAdmin(UserAdmin):
         (None, {
             "classes": ("wide", ),
             "fields": (
-                "email", "password1", "password2", "is_staff",
+                "email", "first_name", "last_name", "password1", "password2", "is_staff",
                 "is_active", "groups", "user_permissions"
             ),
         }),
@@ -35,5 +39,9 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ("email", )
     ordering = ("email", )
     
+
+class CustomerAdmin(admin.ModelAdmin):
+    form = CustomerForm
+
 admin.site.register(CustomUser, CustomUserAdmin)
-    
+admin.site.register(Customer, CustomerAdmin)
