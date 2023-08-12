5c7e43a874f15e44f15671d1f87114743e6e04d8
SaugatMgr <saugatmagar433@gmail.com>
2023-08-12 10:30:44 +0545

    allauth urls

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -20,7 +20,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    # allauth user management
+    path("accounts/", include("allauth.urls")),
+    # Local App Urls
     path("", include("electronics.urls")),
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT,
