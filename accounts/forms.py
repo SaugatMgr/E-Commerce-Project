0f3820eb13390b288754b1ec2522ce8bc524af89
SaugatMgr <saugatmagar433@gmail.com>
2023-08-12 10:32:14 +0545

    extra fields in custom user creation form

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,16 +11,18 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ("email", "first_name", "last_name", )
-        
+        fields = ("email", "first_name", "last_name",
+                  "password1", "password2", )
+
+
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ("email", "first_name", "last_name", )
-        
+
+
 class CustomerForm(forms.ModelForm):
     class Meta:
         widgets = {
             "phone_number": PhoneNumberPrefixWidget(),
         }
-        
