c6fdbe1e2a0f98014faec838d51b858ee44a831d
SaugatMgr <saugatmagar433@gmail.com>
2024-07-08 05:37:06 -0700

    required forms for checkout

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,9 +3,8 @@ from django.contrib.auth.forms import (
     UserChangeForm,
 )
 from django import forms
-from phonenumber_field.widgets import PhoneNumberPrefixWidget
-
 from .models import (
+    Address,
     CustomUser,
     Order,
     Customer,
@@ -15,29 +14,62 @@ from .models import (
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ("email", "first_name", "last_name",
-                  "password1", "password2", )
+        fields = (
+            "email",
+            "first_name",
+            "last_name",
+            "password1",
+            "password2",
+        )
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ("email", "first_name", "last_name", )
+        fields = (
+            "email",
+            "first_name",
+            "last_name",
+        )
+
+
+# This is the form that will be used to create a new user or display the user's information in checkout form.
+class CustomUserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            "email",
+            "first_name",
+            "last_name",
+            "password",
+        )
 
 
 class CheckOutForm(forms.ModelForm):
     class Meta:
         model = Order
-        exclude = ("cart", "order_notes", "status", "customer", )
+        exclude = (
+            "cart",
+            "status",
+            "customer",
+        )
+
 
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         exclude = ("user",)
-        widgets = {
-            "phone_number": PhoneNumberPrefixWidget(
-                attrs={
-                    "class": "form-select",
-                    }
-                ),
-        }
+
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = (
+            "address_line_1",
+            "address_line_2",
+            "city",
+            "state_or_province",
+            "postal_code",
+        )
