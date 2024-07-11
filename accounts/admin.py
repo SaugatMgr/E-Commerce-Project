b3ba526f12a183957803026a30521bcc38421588
SaugatMgr <saugatmagar433@gmail.com>
2024-07-11 09:57:23 -0700

    file formatted

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,11 +7,13 @@ from .forms import (
     CustomerForm,
 )
 from .models import (
+    BillingAddress,
     CustomUser,
     Customer,
     Cart,
     CartItems,
     Order,
+    ShippingAddress,
     WishList,
 )
 
@@ -20,36 +22,68 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ("email", "first_name", "last_name", "is_staff", "is_active", )
-    list_filter = ("email", "first_name", "last_name", "is_staff", "is_active", )
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
     fieldsets = (
-        (None, {
-            "fields": (
-                "email", "first_name", "last_name", "password", 
-            ),
-        }),
-        ("Permissions", {
-            "fields": (
-                "is_staff", "is_active", "groups", "user_permissions"
-            ),
-        }),
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password",
+                ),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": ("is_staff", "is_active", "groups", "user_permissions"),
+            },
+        ),
     )
     add_fieldsets = (
-        (None, {
-            "classes": ("wide", ),
-            "fields": (
-                "email", "first_name", "last_name", "password1", "password2", "is_staff",
-                "is_active", "groups", "user_permissions"
-            ),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
     )
-    search_fields = ("email", )
-    ordering = ("email", )
-    
+    search_fields = ("email",)
+    ordering = ("email",)
+
 
 class CustomerAdmin(admin.ModelAdmin):
     form = CustomerForm
 
+
 admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Customer, CustomerAdmin)
-admin.site.register([Cart, CartItems,Order, WishList])
+admin.site.register(Customer)
+admin.site.register(
+    [Cart, CartItems, Order, WishList, BillingAddress, ShippingAddress]
+)
