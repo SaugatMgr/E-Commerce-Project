cde2c825aca988b864739fefcc6b9bb479af08f2
SaugatMgr <saugatmagar433@gmail.com>
2024-07-11 09:55:12 -0700

    file containing static contents like choices

---
+++ accounts/constants.py
@@ -0,0 +1,18 @@
+ORDER_STATUS_CHOICES = [
+        ("Pending", "Pending"),
+        ("Processing", "Processing"),
+        ("On the Way", "On the Way"),
+        ("Completed", "Completed"),
+        ("Cancelled", "Cancelled"),
+    ]
+PAYMENT_STATUS_CHOICES = [
+    ("UNPAID", "Unpaid"),
+    ("PAID", "Paid"),
+    ("REFUNDED", "Refunded"),
+]
+
+PAYMENT_METHOD_CHOICES = [
+    ("Khalti", "Khalti"),
+    ("Esewa", "Esewa"),
+    ("Cash on Delivery", "Cash on Delivery"),
+]
