55c349557ee458e310e9c129a859559f759e8812
SaugatMgr <saugatmagar433@gmail.com>
2024-07-13 10:07:50 -0700

    context processor for context access across templates

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "electronics.context_processors.electronics_context",
             ],
         },
     },
