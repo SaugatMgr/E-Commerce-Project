db97267649932a57fe94a03fe68e55037e182eb1
SaugatMgr <saugatmagar433@gmail.com>
2024-07-17 19:08:12 +0545

    context for all templates

--- electronics/context_processors.py
+++ electronics/context_processors.py
@@ -1,8 +1,3 @@
-from .models import (
-    Product,
-    Category,
-    SubCategory,
-)
 from accounts.models import (
     Customer,
     Cart,
@@ -13,37 +8,6 @@ from accounts.models import (
 def electronics_context(request):
     context = {}
     user = request.user
-    all_categories = Category.objects.all()
-    all_products = Product.objects.all()
-    first_category = all_categories.first()
-    today_deals_categories = all_categories[1:8]
-
-    filter_product_by_views = Product.objects.filter(views_count__gte=0)
-    filter_product_by_higher_views = filter_product_by_views.order_by("-views_count")
-    products_by_date = filter_product_by_views.order_by("-added_date")
-
-    context["categories"] = all_categories
-    context["product_category"] = all_categories[:9]
-    context["product_category_rem"] = all_categories[9:12]
-    context["product_sub_category"] = SubCategory.objects.all()
-
-    context["today_deals_categories_first"] = first_category
-    context["today_deals_categories"] = today_deals_categories
-    context["today_deals_products"] = products_by_date.filter(category=first_category)
-    context["rem_today_deals_products"] = products_by_date.filter(
-        category__in=today_deals_categories
-    )
-
-    context["best_sellers"] = filter_product_by_higher_views
-    context["all_products"] = all_products
-
-    context["new_products_left_sidebar"] = products_by_date[:2]
-    context["new_products_center"] = products_by_date[2]
-    context["new_products_right_sidebar"] = products_by_date[3:5]
-
-    context["featured_products"] = all_products.filter(is_featured=True)
-    context["default_active_tab_category"] = Category.objects.first()
-    context["remaining_categories"] = all_categories[1:8]
 
     if user.is_authenticated:
         current_customer = Customer.objects.filter(user=user).first()
@@ -57,5 +21,4 @@ def electronics_context(request):
                     context["sub_total"] = sum(
                         [item.quantity * item.product.price for item in cart_items]
                     )
-
-        return context
+    return context
