07aac66424fbf992b8aa9ec64ad97bf0ec322e7b
SaugatMgr <saugatmagar433@gmail.com>
2024-07-17 19:07:43 +0545

    context for main home page

--- electronics/views.py
+++ electronics/views.py
@@ -18,9 +18,11 @@ from django.http import JsonResponse
 from django.db import transaction
 
 from .models import (
+    Category,
     Product,
     Image,
     Review,
+    SubCategory,
 )
 from accounts.models import (
     Customer,
@@ -40,6 +42,39 @@ class HomePageView(ListView):
     model = Product
     template_name = "main/home/home.html"
     context_object_name = "products"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        all_categories = Category.objects.all()
+        all_products = Product.objects.all()
+        filter_product_by_views = Product.objects.filter(views_count__gte=0)
+        filter_product_by_higher_views = filter_product_by_views.order_by(
+            "-views_count"
+        )
+        products_by_date = filter_product_by_views.order_by("-added_date")
+        filter_product_by_categories = products_by_date.filter(
+            category__in=all_categories
+        )
+
+        context["categories"] = all_categories
+        context["product_category"] = all_categories[:9]
+        context["product_category_rem"] = all_categories[9:12]
+        context["product_sub_category"] = SubCategory.objects.all()
+
+        context["today_deals_categories"] = all_categories
+        context["today_deals_products"] = filter_product_by_categories
+
+        context["best_sellers"] = filter_product_by_higher_views
+        context["all_products"] = all_products
+
+        context["new_products_left_sidebar"] = products_by_date[:2]
+        context["new_products_center"] = products_by_date[2]
+        context["new_products_right_sidebar"] = products_by_date[3:5]
+
+        context["featured_products"] = all_products.filter(is_featured=True)
+        context["home_product_area_categories"] = all_categories[0:7]
+
+        return context
 
 
 class ShopItemsView(HomePageView):
