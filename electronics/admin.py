8cc81b410d248d28babcffb5b0166211b11cbf43
SaugatMgr <saugatmagar433@gmail.com>
2024-07-18 16:46:52 +0545

    added tinymce to description field of Product model in admin panel as well

--- electronics/admin.py
+++ electronics/admin.py
@@ -1,3 +1,4 @@
+from tinymce.widgets import TinyMCE
 from django.contrib import admin
 
 from .models import (
@@ -10,5 +11,18 @@ from .models import (
     Contact,
     NewsLetter,
 )
+from django import forms
 
-admin.site.register([Product, Image, Category, SubCategory, Tag, Review, Contact, NewsLetter, ])
+class ProductAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class ProductAdmin(admin.ModelAdmin):
+    form = ProductAdminForm
+
+admin.site.register(Product, ProductAdmin)
+
+admin.site.register([Image, Category, SubCategory, Tag, Review, Contact, NewsLetter, ])
