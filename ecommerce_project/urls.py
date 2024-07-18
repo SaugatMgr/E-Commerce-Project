74887b26350fab1f4c01a7e3c363a5509e58fa88
SaugatMgr <saugatmagar433@gmail.com>
2024-07-18 16:46:12 +0545

    added tinymce urls

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),
     # allauth user management
     path("accounts/", include("allauth.urls")),
     # Local App Urls
