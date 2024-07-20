0c0ba51ba3a4850e3aa77aa5ccf248337a016ab6
SaugatMgr <saugatmagar433@gmail.com>
2024-07-20 15:41:43 +0545

    post list and detail view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,25 @@
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/blog_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.all()
+        return context
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/blog_detail.html"
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        context["post"] = Post.objects.get(id=self.kwargs["post_id"])
+
+        return render(request, self.template_name, context)
