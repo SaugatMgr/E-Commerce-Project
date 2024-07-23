09b95be90715752d507c176b250bc259545f943c
SaugatMgr <saugatmagar433@gmail.com>
2024-07-22 23:54:59 -0700

    Post list and detail view with mixin

--- posts/views.py
+++ posts/views.py
@@ -1,25 +1,40 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from django.db.models import Q
+
+from posts.helpers import PostContextMixin
 
 from .models import Post
 
 
-class PostListView(ListView):
+class PostListView(PostContextMixin, ListView):
     model = Post
-    template_name = "blog/blog_list.html"
+    template_name = "blog/blog.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["posts"] = Post.objects.all()
+        context = self.get_shared_context_data(**context)
+
         return context
 
 
-class PostDetailView(DetailView):
+class PostDetailView(PostContextMixin, DetailView):
     model = Post
     template_name = "blog/blog_detail.html"
 
     def get(self, request, *args, **kwargs):
-        context = {}
-        context["post"] = Post.objects.get(id=self.kwargs["post_id"])
+        context = self.get_shared_context_data(**kwargs)
+        current_post = Post.objects.get(id=self.kwargs["post_id"])
+        context["blg_post_detail"] = current_post
+        context["related_posts"] = (
+            context.get("posts")
+            .filter(
+                Q(category=current_post.category) | Q(tag__in=current_post.tag.all())
+            )
+            .exclude(id=current_post.id)
+            .distinct()
+            # .select_related("category")
+            # .prefetch_related("tag")
+        )
 
         return render(request, self.template_name, context)
