bee8f737545de06d5702b7487ebb0c10263abd52
SaugatMgr <saugatmagar433@gmail.com>
2024-07-22 23:54:23 -0700

    post list and detail urls

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="blog_list"),
-    path("<uuid:post_id>/", PostDetailView.as_view(), name="blog_detail"),
+    path("", PostListView.as_view(), name="post_list"),
+    path("<uuid:post_id>/", PostDetailView.as_view(), name="post_detail"),
 ]
