aa04e93d717b43ef966538c2faea07d672585456
SaugatMgr <saugatmagar433@gmail.com>
2024-07-23 03:53:04 -0700

    added post by category, tag and search urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path
 
-from .views import PostListView, PostDetailView
+from .views import (
+    PostByCategoryView,
+    PostByTagView,
+    PostListView,
+    PostDetailView,
+    PostSearchView,
+)
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post_list"),
-    path("<uuid:post_id>/", PostDetailView.as_view(), name="post_detail"),
+    path("<uuid:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path(
+        "post-by-category/<str:category_name>/",
+        PostByCategoryView.as_view(),
+        name="post_by_category_list",
+    ),
+    path(
+        "post-by-tag/<str:tag_name>/", PostByTagView.as_view(), name="post_by_tag_list"
+    ),
+    path("post-search/", PostSearchView.as_view(), name="post_search"),
 ]
