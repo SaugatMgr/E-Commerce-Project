4d236b50d72003f6fbfbf02204f3db91ef78e511
SaugatMgr <saugatmagar433@gmail.com>
2024-07-29 05:04:10 -0700

    added function to generate absolute url and function to generate hmac signature for esewa payment integration

--- accounts/helpers.py
+++ accounts/helpers.py
@@ -1,4 +1,22 @@
 import uuid
+import hmac
+import hashlib
+import base64
+
+from django.urls import reverse
+
 
 def calc_tracking_number():
     return uuid.uuid4().hex
+
+
+def generate_hmac_signature(secret_key, message):
+    hmac_obj = hmac.new(secret_key.encode(), message.encode(), hashlib.sha256)
+    hmac_signature = hmac_obj.digest()
+    signature = base64.b64encode(hmac_signature).decode()
+
+    return signature
+
+
+def build_abs_uri(request, url_name, *args):
+    return request.build_absolute_uri(reverse(url_name, args=args))
