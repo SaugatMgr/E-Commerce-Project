7e53f44d8988a94b6d043b564faab7de37de57f7
SaugatMgr <saugatmagar433@gmail.com>
2024-07-29 05:03:17 -0700

    added esewa payment and payment verification urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import (
     AddToWishListView,
     CheckOutView,
+    EsewaPaymentVerifyView,
+    EsewaPaymentView,
     KhaltiPaymentVerifyView,
     KhaltiPaymentView,
     UserAccountView,
@@ -16,6 +18,24 @@ urlpatterns = [
     path("wishlist/", WishListView.as_view(), name="wishlist"),
     path("add-to-wishlist/", AddToWishListView.as_view(), name="add_to_wishlist"),
     path("remove-from-wishlist/", remove_wishlist_item, name="remove_from_wishlist"),
-    path("khalti-payment/<uuid:order_id>/", KhaltiPaymentView.as_view(), name="khalti_payment"),
-    path("khalti-payment/verify/", KhaltiPaymentVerifyView.as_view(), name="khalti_payment_verify"),
+    path(
+        "khalti-payment/<uuid:order_id>/",
+        KhaltiPaymentView.as_view(),
+        name="khalti_payment",
+    ),
+    path(
+        "khalti-payment/verify/",
+        KhaltiPaymentVerifyView.as_view(),
+        name="khalti_payment_verify",
+    ),
+    path(
+        "esewa-payment/<uuid:order_id>/",
+        EsewaPaymentView.as_view(),
+        name="esewa_payment",
+    ),
+    path(
+        "esewa-payment/verify/<uuid:order_id>/",
+        EsewaPaymentVerifyView.as_view(),
+        name="esewa_payment_verify",
+    ),
 ]
