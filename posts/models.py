d7f4406289659fd83037900ccf48f4f6d9fc086d
SaugatMgr <saugatmagar433@gmail.com>
2024-08-08 08:51:20 -0700

    removed likes dislikes field from post, comment and reply models then added new like dislike model for all the models respectively

--- posts/models.py
+++ posts/models.py
@@ -54,8 +54,6 @@ class Post(CommonInfo):
         null=True,
     )
     tag = models.ManyToManyField("PostTag", related_name="posts", blank=True)
-    likes = models.PositiveIntegerField(default=0, blank=True)
-    dislikes = models.PositiveIntegerField(default=0, blank=True)
     views = models.PositiveBigIntegerField(default=0, blank=True)
     published_on = models.DateTimeField(null=True, blank=True)
     featured_image = models.ImageField(
@@ -67,6 +65,14 @@ class Post(CommonInfo):
 
     def __str__(self):
         return f"Post by {self.author} - {self.title[:20]}"
+
+    @property
+    def like_count(self):
+        return self.postlikes.count()
+
+    @property
+    def dislike_count(self):
+        return self.postdislikes.count()
 
 
 class Comment(CommonInfo):
@@ -81,8 +87,6 @@ class Comment(CommonInfo):
         on_delete=models.CASCADE,
     )
     comment = models.TextField()
-    likes = models.PositiveIntegerField(default=0, blank=True)
-    dislikes = models.PositiveIntegerField(default=0, blank=True)
     is_pinned = models.BooleanField(default=False)
 
     class Meta:
@@ -90,6 +94,14 @@ class Comment(CommonInfo):
 
     def __str__(self):
         return f"Comment by {self.comment_writer} on {self.post.title[:20]}"
+
+    @property
+    def like_count(self):
+        return self.commentlikes.count()
+
+    @property
+    def dislike_count(self):
+        return self.commentdislikes.count()
 
 
 class Reply(CommonInfo):
@@ -109,13 +121,139 @@ class Reply(CommonInfo):
         related_name="replies",
     )
     reply_content = models.TextField()
-    likes = models.PositiveIntegerField(default=0, blank=True)
-    dislikes = models.PositiveIntegerField(default=0, blank=True)
 
     class Meta:
         ordering = ["-created_at"]
+        verbose_name = "Reply"
+        verbose_name_plural = "Replies"
 
     def __str__(self):
         if self.comment:
             return f"Reply by {self.reply_writer} on {self.comment.post.title[:20]}"
         return f"Reply by {self.reply_writer} on {self.parent.reply_writer}'s comment"
+
+    @property
+    def like_count(self):
+        return self.replylikes.count()
+
+    @property
+    def dislike_count(self):
+        return self.replydislikes.count()
+
+
+class LikeDislikeCommonInfo(CommonInfo):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        action = self.get_action()
+        related_obj = self.get_related_object()
+        return f"{action} by {self.user} on {related_obj}"
+
+    def get_related_object(self):
+        raise NotImplementedError("Subclasses must implement `get_related_object`.")
+
+    def get_action(self):
+        raise NotImplementedError("Subclasses must implement `get_action`.")
+
+
+class PostLikes(LikeDislikeCommonInfo):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="postlikes")
+
+    class Meta:
+        unique_together = ["post", "user"]
+        verbose_name = "Post like"
+        verbose_name_plural = "Post likes"
+
+    def get_related_object(self):
+        return self.post.title[:20]
+
+    def get_action(self):
+        return "Like"
+
+
+class PostDislikes(LikeDislikeCommonInfo):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="postdislikes"
+    )
+
+    class Meta:
+        unique_together = ["post", "user"]
+        verbose_name = "Post dislike"
+        verbose_name_plural = "Post dislikes"
+
+    def get_related_object(self):
+        return self.post.title[:20]
+
+    def get_action(self):
+        return "Dislike"
+
+
+class CommentLikes(LikeDislikeCommonInfo):
+    comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, related_name="commentlikes"
+    )
+
+    class Meta:
+        unique_together = ["comment", "user"]
+        verbose_name = "Comment like"
+        verbose_name_plural = "Comment likes"
+
+    def get_related_object(self):
+        return self.comment.post.title[:20]
+
+    def get_action(self):
+        return "Like"
+
+
+class CommentDislikes(LikeDislikeCommonInfo):
+    comment = models.ForeignKey(
+        Comment, on_delete=models.CASCADE, related_name="commentdislikes"
+    )
+
+    class Meta:
+        unique_together = ["comment", "user"]
+        verbose_name = "Comment dislike"
+        verbose_name_plural = "Comment dislikes"
+
+    def get_related_object(self):
+        return self.comment.post.title[:20]
+
+    def get_action(self):
+        return "Dislike"
+
+
+class ReplyLikes(LikeDislikeCommonInfo):
+    reply = models.ForeignKey(
+        Reply, on_delete=models.CASCADE, related_name="replylikes"
+    )
+
+    class Meta:
+        unique_together = ["reply", "user"]
+        verbose_name = "Reply like"
+        verbose_name_plural = "Reply likes"
+
+    def get_related_object(self):
+        return self.reply.comment.post.title[:20]
+
+    def get_action(self):
+        return "Like"
+
+
+class ReplyDislikes(LikeDislikeCommonInfo):
+    reply = models.ForeignKey(
+        Reply, on_delete=models.CASCADE, related_name="replydislikes"
+    )
+
+    class Meta:
+        unique_together = ["reply", "user"]
+        verbose_name = "Reply dislike"
+        verbose_name_plural = "Reply dislikes"
+
+    def get_related_object(self):
+        return self.reply.comment.post.title[:20]
+
+    def get_action(self):
+        return "Dislike"
