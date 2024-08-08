e3db1826a8b8125171ca9b458f2bd61173fe0c31
SaugatMgr <saugatmagar433@gmail.com>
2024-08-08 08:53:05 -0700

    Like Dislike view to handle all post, comment and reply model like dislike logic as one

--- posts/views.py
+++ posts/views.py
@@ -12,7 +12,17 @@ from django.template.loader import render_to_string
 
 from helpers.pagination import PaginationMixin
 
-from .models import Post, Comment, Reply
+from .models import (
+    CommentDislikes,
+    CommentLikes,
+    Post,
+    Comment,
+    PostDislikes,
+    PostLikes,
+    Reply,
+    ReplyDislikes,
+    ReplyLikes,
+)
 
 
 class PostListView(PaginationMixin, ListView):
@@ -227,6 +237,109 @@ class ReplyView(View):
                         {
                             "success": False,
                             "message": "Reply cannot be empty.",
+                        },
+                        status=400,
+                    )
+            else:
+                return JsonResponse(
+                    {
+                        "success": False,
+                        "message": "Cannot process.Must be and AJAX XMLHttpRequest.",
+                    },
+                    status=400,
+                )
+
+
+class LikeDislikeView(View):
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        user = request.user
+
+        obj_id = data.get("obj_id")
+        model = data.get("model")
+        action = data.get("action")
+
+        with transaction.atomic():
+            if request.headers.get("X-Requested-With") == "XMLHttpRequest":
+                if model == "post":
+                    obj = Post.objects.get(id=obj_id)
+                    like_model = PostLikes
+                    dislike_model = PostDislikes
+                elif model == "comment":
+                    obj = Comment.objects.get(id=obj_id)
+                    like_model = CommentLikes
+                    dislike_model = CommentDislikes
+                elif model == "reply":
+                    obj = Reply.objects.get(id=obj_id)
+                    like_model = ReplyLikes
+                    dislike_model = ReplyDislikes
+                else:
+                    return JsonResponse(
+                        {
+                            "success": False,
+                            "message": "Missing model.",
+                        },
+                        status=400,
+                    )
+                if action == "like":
+                    like_model_obj = like_model.objects.filter(
+                        user=user, **{f"{model}": obj}
+                    )
+                    if like_model_obj.exists():
+                        like_model_obj.delete()
+                        return JsonResponse(
+                            {
+                                "success": True,
+                                "like_count": obj.like_count,
+                                "dislike_count": obj.dislike_count,
+                            },
+                            status=200,
+                        )
+                    else:
+                        like_model.objects.create(user=user, **{f"{model}": obj})
+                        dislike_model.objects.filter(
+                            user=user, **{f"{model}": obj}
+                        ).delete()
+                        return JsonResponse(
+                            {
+                                "success": True,
+                                "like_count": obj.like_count,
+                                "dislike_count": obj.dislike_count,
+                            },
+                            status=201,
+                        )
+                elif action == "dislike":
+                    dislike_model_obj = dislike_model.objects.filter(
+                        user=user, **{f"{model}": obj}
+                    )
+                    if dislike_model_obj.exists():
+                        dislike_model_obj.delete()
+                        return JsonResponse(
+                            {
+                                "success": True,
+                                "like_count": obj.like_count,
+                                "dislike_count": obj.dislike_count,
+                            },
+                            status=200,
+                        )
+                    else:
+                        dislike_model.objects.create(user=user, **{f"{model}": obj})
+                        like_model.objects.filter(
+                            user=user, **{f"{model}": obj}
+                        ).delete()
+                        return JsonResponse(
+                            {
+                                "success": True,
+                                "like_count": obj.like_count,
+                                "dislike_count": obj.dislike_count,
+                            },
+                            status=201,
+                        )
+                else:
+                    return JsonResponse(
+                        {
+                            "success": False,
+                            "message": "Unknown action.",
                         },
                         status=400,
                     )
